686d56259377957fb9cd237cf863c86d8f244fcc
Avantika <avantikasharmaddn22@gmail.com>
2023-02-24 18:31:16 +0530

    first commit

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className = "navbar" >
+            <h1> its my navigation bar</h1>
+            <div className = "app" >
+                <a href = "/" > Home </a>
+                <a href = "/" > Details </a>
+                <a href = "/" > Search </a>
+            </div>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
